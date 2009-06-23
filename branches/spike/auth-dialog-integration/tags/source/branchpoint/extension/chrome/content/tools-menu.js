396cbd3c60ea4f3c6b952ae243bb590e442d4613
travis.illig <travis.illig@a01bd0ea-5b57-11de-8abc-d1c337b90d21>
2009-06-23 15:29:46 +0000

    Created branchpoint for spike codeline.

---
+++ branches/spike/auth-dialog-integration/tags/source/branchpoint/extension/chrome/content/tools-menu.js
@@ -0,0 +1,7 @@
+if(!com.paraesthesia.ntlmauth.ToolsMenu) com.paraesthesia.ntlmauth.ToolsMenu =
+{
+	showEditorDialog: function()
+	{
+		window.open("chrome://ntlmauth/content/edit-dialog.xul", "ntlmauth-edit-dialog", "chrome,alwaysRaised=yes,centerscreen=yes,dependent=yes,modal=yes,dialog=yes,resizable=no");
+	}
+};
