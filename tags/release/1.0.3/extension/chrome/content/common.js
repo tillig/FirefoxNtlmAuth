a7b760531dce09921ddbbd1e0a84e2ccf357733a
travis.illig <travis.illig@a01bd0ea-5b57-11de-8abc-d1c337b90d21>
2010-01-21 19:53:33 +0000

    Created tag for 1.0.3 release.

---
+++ tags/release/1.0.3/extension/chrome/content/common.js
@@ -0,0 +1,82 @@
+if (!com) var com = {};
+if (!com.paraesthesia) com.paraesthesia = {};
+if (!com.paraesthesia.ntlmauth) com.paraesthesia.ntlmauth = {};
+
+if (!com.paraesthesia.ntlmauth.Logger) com.paraesthesia.ntlmauth.Logger =
+{
+	logger: Components.classes['@mozilla.org/consoleservice;1'].getService(Components.interfaces.nsIConsoleService),
+	write: function(msg) {
+		this.logger.logStringMessage(msg);
+	},
+
+	dumpHierarchy: function(obj) {
+		for (var i in obj) {
+			this.write(i + " = " + obj[i]);
+		}
+	}
+};
+
+if (!com.paraesthesia.ntlmauth.String) com.paraesthesia.ntlmauth.String =
+{
+	getHostFromUrl: function(url) {
+		var index = url.indexOf("://");
+		if (index < 0) {
+			return url;
+		}
+		var site = url.substring(index + 3);
+		return site;
+	},
+
+	isValidUrl: function(url) {
+		var urlPattern = /^(?:(?:ftp|https?):\/\/)[^\s]+$/;
+		return urlPattern.test(url.toLowerCase());
+	},
+
+	trim: function(stringToTrim) {
+		var str = stringToTrim.replace(/^\s\s*/, '');
+		var ws = /\s/;
+		var i = str.length;
+		while (ws.test(str.charAt(--i)));
+		return str.slice(0, i + 1);
+	},
+
+	urlSort: function(a, b) {
+		var siteA = com.paraesthesia.ntlmauth.String.getHostFromUrl(a);
+		var siteB = com.paraesthesia.ntlmauth.String.getHostFromUrl(b);
+		if (siteA < siteB) {
+			return -1;
+		}
+		else if (siteA > siteB) {
+			return 1;
+		}
+		return 0;
+	}
+};
+
+if (!com.paraesthesia.ntlmauth.Preferences) com.paraesthesia.ntlmauth.Preferences =
+{
+	preferenceKey: "network.automatic-ntlm-auth.trusted-uris",
+	preferenceService: Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService),
+
+	loadSiteList: function() {
+		var prefString = this.preferenceService.getCharPref(this.preferenceKey);
+		var trimmedArray = new Array();
+		if (prefString != null && prefString.length > 0) {
+			var prefArray = new Array();
+			prefArray = prefString.split(",");
+			for (var i = 0; i < prefArray.length; i++) {
+				var url = com.paraesthesia.ntlmauth.String.trim(prefArray[i]);
+				if (url != null && url.length > 0) {
+					trimmedArray.push(url);
+				}
+			}
+			trimmedArray.sort(com.paraesthesia.ntlmauth.String.urlSort);
+		}
+		return trimmedArray;
+	},
+
+	saveSiteList: function(siteList) {
+		siteList.sort(com.paraesthesia.ntlmauth.String.urlSort);
+		this.preferenceService.setCharPref(this.preferenceKey, siteList.join());
+	}
+};
---
+++ tags/release/1.0.3/extension/chrome/content/edit-dialog.js
@@ -0,0 +1,115 @@
+/// <reference path="~/chrome/content/common.js" />
+if (!com) var com = {};
+if (!com.paraesthesia) com.paraesthesia = {};
+if (!com.paraesthesia.ntlmauth) com.paraesthesia.ntlmauth = {};
+if (!com.paraesthesia.ntlmauth.EditDialog) com.paraesthesia.ntlmauth.EditDialog = {};
+
+if (!com.paraesthesia.ntlmauth.EditDialog.Listbox) com.paraesthesia.ntlmauth.EditDialog.Listbox =
+{
+	clear: function(lbox) {
+		var currentCount = lbox.itemCount;
+		for (var i = 0; i < currentCount; i++) {
+			lbox.removeItemAt(0);
+		}
+	},
+
+	contains: function(lbox, value) {
+		for (var i = 0; i < lbox.itemCount; i++) {
+			var listItem = lbox.getItemAtIndex(i);
+			if (listItem.label.toLowerCase() == value.toLowerCase()) {
+				return true;
+			}
+		}
+		return false;
+	},
+
+	databind: function(lbox, data) {
+		this.clear(lbox);
+		for (var i = 0; i < data.length; i++) {
+			lbox.appendItem(data[i]);
+		}
+	},
+
+	removeSelectedItem: function(lbox) {
+		lbox.removeItemAt(lbox.getIndexOfItem(lbox.selectedItem));
+	},
+
+	toArray: function(lbox) {
+		var list = new Array(lbox.itemCount);
+		for (var i = 0; i < lbox.itemCount; i++) {
+			var listItem = lbox.getItemAtIndex(i);
+			list[i] = listItem.label;
+		}
+		return list;
+	}
+};
+
+if (!com.paraesthesia.ntlmauth.EditDialog.DialogController) com.paraesthesia.ntlmauth.EditDialog.DialogController =
+{
+	addButton: null,
+	addSiteTextBox: null,
+	removeButton: null,
+	siteListBox: null,
+
+	addSite: function() {
+		var url = this.addSiteTextBox.value;
+		if (!com.paraesthesia.ntlmauth.EditDialog.Listbox.contains(this.siteListBox, url)) {
+			var newSiteList = com.paraesthesia.ntlmauth.EditDialog.Listbox.toArray(this.siteListBox);
+			newSiteList[newSiteList.length] = url;
+			com.paraesthesia.ntlmauth.Preferences.saveSiteList(newSiteList);
+		}
+		this.populateSiteList();
+		this.addSiteTextBox.value = "";
+		this.updateAddButtonDisabled(this.addSiteTextBox);
+	},
+
+	help: function() {
+		window.open("http://code.google.com/p/firefox-ntlmauth/wiki/AddonHelp");
+	},
+
+	initialize: function() {
+		this.addButton = document.getElementById("addButton");
+		this.addSiteTextBox = document.getElementById("addSiteTextBox");
+		this.removeButton = document.getElementById("removeButton");
+		this.siteListBox = document.getElementById("siteListBox");
+
+		this.addSiteTextBox.focus();
+		this.populateSiteList();
+		// TODO: Put the current web site in the textbox IF it's not already in the list.
+		this.updateAddButtonDisabled(this.addSiteTextBox);
+
+	},
+
+	onAddTextboxChanged: function(textbox) {
+		this.updateAddButtonDisabled(textbox);
+	},
+
+	onAddTextboxKeyUp: function(event) {
+		if (event.keyCode == KeyEvent.DOM_VK_RETURN && !this.addButton.disabled) {
+			this.addSite();
+		}
+	},
+
+	populateSiteList: function() {
+		var list = com.paraesthesia.ntlmauth.Preferences.loadSiteList();
+		com.paraesthesia.ntlmauth.EditDialog.Listbox.databind(this.siteListBox, list);
+	},
+
+	removeSite: function() {
+		com.paraesthesia.ntlmauth.EditDialog.Listbox.removeSelectedItem(this.siteListBox);
+		var newSiteList = com.paraesthesia.ntlmauth.EditDialog.Listbox.toArray(this.siteListBox);
+		com.paraesthesia.ntlmauth.Preferences.saveSiteList(newSiteList);
+
+		this.populateSiteList();
+		this.updateRemoveButtonDisabled(this.siteListBox);
+	},
+
+	updateAddButtonDisabled: function(textbox) {
+		var url = textbox.value;
+		this.addButton.disabled = !com.paraesthesia.ntlmauth.String.isValidUrl(url);
+	},
+
+	updateFormFromSelection: function(listbox) {
+		this.removeButton.disabled = (this.siteListBox.selectedItem == null);
+	}
+};
